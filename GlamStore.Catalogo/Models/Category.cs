8e576f67fbdb199352679f2c9bb31c9a7e165357
Gabriel Lamas <glamas.2003@gmail.com>
2024-06-11 13:51:26 -0300

    Firts Commit

---
+++ GlamStore.Catalogo/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace GlamStore.Catalogo.Models;
+
+public class Category
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Manufacturer { get; set; }
+    public string? ImageUrl { get; set; }
+    
+}
---
+++ GlamStore.Catalogo/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace GlamStore.Catalogo.Models;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string ImageUrl { get; set; }
+    public float Stock { get; set; }
+
+}
